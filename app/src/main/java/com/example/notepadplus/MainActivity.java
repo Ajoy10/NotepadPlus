6744177a6c185eb9bfaa9dd6ef59215287e3535e
Ajoy10 <achu.ajoy10@gmail.com>
2023-10-02 19:21:05 +0530

    XML for Main Activity, Edit Note Activity done

--- app/src/main/java/com/example/notepadplus/MainActivity.java
+++ app/src/main/java/com/example/notepadplus/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.notepadplus;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // For custom actionbar
+        Toolbar toolbar = findViewById(R.id.mainToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false); // Removes the title from the action bar
+
+        Intent editText = new Intent(this, NoteEditActivity.class);
+        startActivity(editText);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        menu.getItem(0).getIcon().setTint(getColor(R.color.d_faded_md)); // Setting the tint of the button
+        return super.onCreateOptionsMenu(menu);
     }
 }
